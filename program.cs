de542182d944e0c2c9f791b97348e4606cd07787
dainq <97323194+dainqd@users.noreply.github.com>
2022-08-04 14:51:07 +0700

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace task1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<Product> inv = new List<Product>;
+            inv.Add(new Product("A", 5.9, 3));
+            inv.Add(new Product("A", 8.2, 2));
+
+            inv.Add(new Product("A", 3.5, 4);
+            inv.Add(new Product("A", 1.8, 8));
+            Console.WriteLine("Product List: ");
+            foreach(Product i in inv)
+            {
+                Console.WriteLine(" " + i);
+            }
+        }
+    }
+}
+
